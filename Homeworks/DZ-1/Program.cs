1f36a70c63be7f8829fdb145848d6822e0ee23c6
Surkova Maria <garbeag@yandex>
2023-04-24 11:49:52 +0300

    Домашнее задание 3

--- Homeworks/DZ-1/Program.cs
+++ Homeworks/DZ-1/Program.cs
@@ -3,7 +3,6 @@
 // int Number1 = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine("Введите 2 число");
 // int Number2 = Convert.ToInt32(Console.ReadLine());
-
 // if(Number1 > Number2)
 // {
 //         Console.WriteLine($"a = {Number1}; b = {Number2} -> Max = {Number1}");}
@@ -13,21 +12,31 @@
 
 
 // Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
-Console.WriteLine ("Введите 1 число"); 
-int Number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите 2 число");
-int Number2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите 3 число");
-int Number3 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Введите 1 число"); 
+// int Number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите 2 число");
+// int Number2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите 3 число");
+// int Number3 = Convert.ToInt32(Console.ReadLine());
 
-if(Number1 > Number2)
-{
-        if (Number1 > Number3)
-             {Console.WriteLine($"{Number1}, {Number2}, {Number3}  -> Max = {Number1}");}
-    else
-       {Console.WriteLine($"{Number1}, {Number2}, {Number3}  -> Max = {Number3}");}}
-  else if (Number2 > Number3)
-   {Console.WriteLine($"{Number1}, {Number2}, {Number3} -> Max = {Number2}");} 
-    else 
-     {Console.WriteLine($"{Number1}, {Number2}, {Number3}  -> Max = {Number3}");} 
+// if(Number1 > Number2)
+// {
+//         if (Number1 > Number3)
+//              {Console.WriteLine($"{Number1}, {Number2}, {Number3}  -> Max = {Number1}");}
+//     else
+//        {Console.WriteLine($"{Number1}, {Number2}, {Number3}  -> Max = {Number3}");}}
+//   else if (Number2 > Number3)
+//    {Console.WriteLine($"{Number1}, {Number2}, {Number3} -> Max = {Number2}");} 
+//     else 
+//      {Console.WriteLine($"{Number1}, {Number2}, {Number3}  -> Max = {Number3}");} 
   
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+Console.WriteLine ("Введите число"); 
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if(Number % 2 == 0)
+{
+        Console.WriteLine($" {Number} -> Да");}
+else
+{
+    Console.WriteLine($"{Number}-> Нет");}
