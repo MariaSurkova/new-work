2771f523f6cd3b0966767e64c094450d00a85ac7
Surkova Maria <garbeag@yandex>
2023-04-24 11:27:55 +0300

    Домашнее задание 2

--- Homeworks/DZ-1/Program.cs
+++ Homeworks/DZ-1/Program.cs
@@ -1,12 +1,33 @@
 ﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Console.WriteLine ("Введите 1 число"); 
+// int Number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите 2 число");
+// int Number2 = Convert.ToInt32(Console.ReadLine());
+
+// if(Number1 > Number2)
+// {
+//         Console.WriteLine($"a = {Number1}; b = {Number2} -> Max = {Number1}");}
+// else
+// {
+//     Console.WriteLine($"a = {Number1}; b = {Number2} -> Max = {Number2}");}
+
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
 Console.WriteLine ("Введите 1 число"); 
 int Number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите 2 число");
 int Number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3 число");
+int Number3 = Convert.ToInt32(Console.ReadLine());
 
 if(Number1 > Number2)
 {
-        Console.WriteLine($"a = {Number1}; b = {Number2} -> Max = {Number1}");}
-else
-{
-    Console.WriteLine($"a = {Number1}; b = {Number2} -> Max = {Number2}");}
+        if (Number1 > Number3)
+             {Console.WriteLine($"{Number1}, {Number2}, {Number3}  -> Max = {Number1}");}
+    else
+       {Console.WriteLine($"{Number1}, {Number2}, {Number3}  -> Max = {Number3}");}}
+  else if (Number2 > Number3)
+   {Console.WriteLine($"{Number1}, {Number2}, {Number3} -> Max = {Number2}");} 
+    else 
+     {Console.WriteLine($"{Number1}, {Number2}, {Number3}  -> Max = {Number3}");} 
+  
